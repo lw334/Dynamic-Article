4905901d4498c4a1b829ca828392435aa52540ca
weiwei334 <lingweic@uchicago.edu>
2015-08-17 13:32:14 -0500

    more legend and see changes

--- js/slideScatter.js
+++ js/slideScatter.js
@@ -2,15 +2,16 @@ $(function getStep(){
 	var graphStep;
 	$("#steps").click(function(event) {
 	    graphStep = $(event.target).text();
+	    $('#steps li').removeClass('active-step');
+	    var currentStep = "#" + "step" + graphStep;
+	    $(currentStep).addClass("active-step");
 	    $('.text-blocks-scatter p').removeClass('active-explanation');
 		$("#rect-step2").attr("class", "rect-graph");
 		$("#rect-step3").attr("class", "rect-graph");
 		$("#rect-step4").attr("class", "rect-graph");
-
-	    var currentExp = "#"+"explanation"+graphStep;
+	    var currentExp = "#" + "explanation" + graphStep;
 	    $(currentExp).addClass("active-explanation");
-
-	    var currentRect = "#"+"rect-step"+graphStep;
+	    var currentRect = "#" + "rect-step" + graphStep;
 	    $(currentRect).attr("class", "active-graph");
 	});
 });
